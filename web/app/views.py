11c9f37dc8d38c45642b3bc4781b785b39fc3362
vlyubin <vlyubin@gmail.com>
2014-03-01 12:58:54 -0800

    More basics

--- web/app/views.py
+++ web/app/views.py
@@ -5,10 +5,29 @@ from flask.ext.sqlalchemy import SQLAlchemy
 import datetime
 import os, json
 
+@app.route('/login')
+def login():
+	return render_template('login.html')
+
+@app.route('/dashboard')
+def dashboard():
+	return render_template('dashboard.html')
+
+@app.route('/view/<user_id>')
+def view():
+	# View activity of a particular user
+	return render_template('view.html')
+
+@app.route('/add')
+def add():
+	# Admins can add people to user list or admin list
+	return render_template('add.html')
+
 @app.route('/')
 def home():
+	# If logged in, redirect to main dashboard, otherwise
+	# redirect to login page
 	return "Hello world!"
-
 
 #################################
 # Error handlers
