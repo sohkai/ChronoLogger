93408d7efa274cbe4fcfeae8a95902f9cf5010ab
vlyubin <vlyubin@gmail.com>
2014-03-01 13:20:28 -0800

    Stuff

--- web/app/views.py
+++ web/app/views.py
@@ -5,8 +5,14 @@ from flask.ext.sqlalchemy import SQLAlchemy
 import datetime
 import os, json
 
+@app.route('/logout')
+def logout():
+	session['chrono_token'] = ''
+	return redirect('/login')
+
 @app.route('/login')
 def login():
+	session['chrono_token'] = 'fake'
 	return render_template('login.html')
 
 @app.route('/dashboard')
@@ -23,11 +29,41 @@ def add():
 	# Admins can add people to user list or admin list
 	return render_template('add.html')
 
+def is_loggedin():
+	return 'chrono_token' in session and session['chrono_token'] != ''
+
 @app.route('/')
 def home():
 	# If logged in, redirect to main dashboard, otherwise
 	# redirect to login page
-	return "Hello world!"
+	if is_loggedin():
+		return redirect('/dashboard')
+	else:
+		return redirect('/login')
+
+#################################
+# API for Android app
+#################################
+
+@app.route('/checkin')
+def checkin():
+	mail = request.get('mail')
+	beacon_string = request.get('beacon')
+	return "TODO"
+
+@app.route('/leave')
+def leave():
+	mail = request.get('mail')
+	beacon_string = request.get('beacon')
+	return "TODO"
+
+@app.route('/getstatus')
+def get_status():
+	return "TODO"
+
+@app.route('/getvisits')
+def get_visits():
+	return "TODO"
 
 #################################
 # Error handlers
